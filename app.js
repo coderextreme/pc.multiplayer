37b9ababa73f167a261e8b5e908bf99cb2cec071
John Carlson <john.carlson3@sbcglobal.net>
2014-12-30 16:49:48 -0600

    moved packages around, added grunt

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http').Server(express);
 var io = require('socket.io')(http);
 
 express.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/public/index.html');
 });
 
 function Multiplayer() {
