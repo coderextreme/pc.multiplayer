c2d3a5aee38404a5fc1516a54ce7dd42231f5e49
John Carlson <john.carlson3@sbcglobal.net>
2014-12-30 15:57:12 -0600

    added __dirname, made packages private.  changed which layers held z-index

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http').Server(express);
 var io = require('socket.io')(http);
 
 express.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile(__dirname + '/index.html');
 });
 
 function Multiplayer() {
