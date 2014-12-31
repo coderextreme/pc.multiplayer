083187cadd56052471ca717c313ea926aac2fdd5
John Carlson <john.carlson3@sbcglobal.net>
2014-12-31 11:49:41 -0600

    added a test

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,20 @@
 QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+	assert.expect(2);
+	var options = {
+            transports: ['websocket'],
+            'force new connection': true
+        };
+ 
+	var client = io.connect("http://localhost:8080", options);
+	assert.ok(client !== null, "Passed!");
+ 
+        client.once("connect", function () {
+            client.once("servermessage", function (message) {
+		assert.ok( message == "0 joined.", "Passed!" );
+ 
+                client.disconnect();
+            });
+ 
+	    client.emit('clientrejoin', location.href);
+        });
 });
