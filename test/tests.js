14117883efae81c8a0488d1110df86b4c29946f2
John Carlson <john.carlson3@sbcglobal.net>
2014-12-31 12:20:54 -0600

    fixed async test

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,18 @@
-QUnit.test( "hello test", function( assert ) {
-	assert.async()
+QUnit.test( "join test", function( assert ) {
+	var done = assert.async();
 	var options = {
             transports: ['websocket'],
             'force new connection': true
         };
  
 	var client = io.connect("http://localhost:8080", options);
-	assert.ok(client !== null, "io connect!");
  
         client.once("connect", function () {
             client.once("servermessage", function (message) {
-		assert.ok( message.substring(2,9) == "joined.", "client joined!" );
+		assert.ok( message.substring(2,9) === "joined.", "Passed!");
  
                 client.disconnect();
+		done();
             });
  
 	    client.emit('clientrejoin', location.href);
