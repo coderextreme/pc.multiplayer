ab49d2a375e3d03d1268c1a055aa24da1c38a151
John Carlson <john.carlson3@sbcglobal.net>
2015-01-02 11:56:32 -0600

    added another test and moved client connect outside of tests

--- tests/pcmtests.js
+++ tests/pcmtests.js
@@ -16,10 +16,10 @@ describe( "join test", function() {
 	server = require('../app');
 	done();
     });
+    var client = io.connect("http://localhost:8088", options);
  
  
     it("should rejoin the server and get a join message back", function (done) {
-	var client = io.connect("http://localhost:8088", options);
         client.once("connect", function () {
             client.once("servercapability", function (message) {
 		console.log(message);
@@ -27,11 +27,28 @@ describe( "join test", function() {
             client.once("servermessage", function (message) {
 		message.should.equal("0 joined.");
  
-                client.disconnect();
 		done();
             });
  
 	    client.emit('clientrejoin', "GOOBERS");
         });
+    });
+
+
+    it("should send 2 client move messages and receive a server updates", function (done) {
+            client.once("serverupdate", function (player, position, orientation) {
+		player.should.equal(0);
+		JSON.stringify(position).should.equal(JSON.stringify([0,0,0]));
+		JSON.stringify(orientation).should.equal(JSON.stringify([0,0,0]));
+                client.once("serverupdate", function (player, position, orientation) {
+			player.should.equal(0);
+			JSON.stringify(position).should.equal(JSON.stringify([1,0,0]));
+			JSON.stringify(orientation).should.equal(JSON.stringify([1,0,0]));
+			client.disconnect();
+			done();
+		});
+	        client.emit('clientmove', [1,0,0,1,0,0]);
+            });
+	    client.emit('clientmove', [0,0,0,0,0,0]);
     });
 });
