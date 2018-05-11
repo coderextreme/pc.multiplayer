07d920b03a2ea25dba1adf664628f5e5c5192782
John Carlson <yottzumm@gmail.com>
2018-05-11 07:32:25 -0400

    now seems to move marker

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -30,7 +30,7 @@ Player.prototype = {
 				players[playernumber].marker = WE.marker(newLatLngPN).addTo(map);
 			}
 		}
-		onLocationfound = function(e){
+		//onLocationfound = function(e){
 			for (var player in players) {
 				var newLatLng = new L.LatLng(players[player].position[1], players[player].position[0], players[player].position[2]);
 				console.log("update "+player+" "+players[player].position);
@@ -38,10 +38,9 @@ Player.prototype = {
 				players[player].marker = WE.marker(newLatLng).addTo(map);
 
 			}
-		}
-		//map.on('mousedown', onLocationfound);
-		onLocationfound();
-		map.on('locationfound', onLocationfound);
+		//}
+		//onLocationfound();
+		// map.on('locationfound', onLocationfound);
 		if (thisplayer == playernumber) {
 			if (position[0] === 0 && position[1] === 0 && position[2] === 0) {
 				// alert("Beginning again");
